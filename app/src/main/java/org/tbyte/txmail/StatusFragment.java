d5e1fc61bd718a142a4e10fea32cd19093478074
Daniel Gondos <dan.gondos@gmail.com>
2015-05-07 00:45:44 +1000

    SMS triggering and SMTP integration
    SyncService is now a BroadcastReceiver triggered by an incoming SMS.
    
    In onReceive, SyncService now sends an email to a test server.
    
    Added kore-javamail library to send mails with, and created SMTPSender wrapper for it.

--- app/src/main/java/org/tbyte/txmail/StatusFragment.java
+++ app/src/main/java/org/tbyte/txmail/StatusFragment.java
@@ -1,20 +1,12 @@
 package org.tbyte.txmail;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Calendar;
 
 /**
  * Created by dgondos on 2/05/15.
@@ -40,8 +32,8 @@ public class StatusFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_tx_mail, container, false);
         EditText statusLog = (EditText) rootView.findViewById(R.id.statusLog);
         statusLog.setText("This is view status");
-        Button startButton = (Button) rootView.findViewById(R.id.startButton);
-        startButton.setOnClickListener(startButtonClickListener());
+//        Button startButton = (Button) rootView.findViewById(R.id.startButton);
+//        startButton.setOnClickListener(startButtonClickListener());
         return rootView;
     }
 
@@ -49,20 +41,20 @@ public class StatusFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlarmManager alarmMgr = (AlarmManager)v.getContext().getSystemService(Context.ALARM_SERVICE);
-                Intent intent = new Intent(v.getContext(), SyncService.class);
-                PendingIntent alarmIntent = PendingIntent.getBroadcast(v.getContext(), 0, intent, 0);
-
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-
-                alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                        1000 * 5, alarmIntent);
-                Log.d("txmail sync", "alarm registered");
-
-                v.setOnClickListener(stopButtonClickListener());
-                Button thisButton = (Button)v;
-                thisButton.setText(R.string.action_stop);
+//                AlarmManager alarmMgr = (AlarmManager)v.getContext().getSystemService(Context.ALARM_SERVICE);
+//                Intent intent = new Intent(v.getContext(), SyncService.class);
+//                PendingIntent alarmIntent = PendingIntent.getBroadcast(v.getContext(), 0, intent, 0);
+//
+//                Calendar calendar = Calendar.getInstance();
+//                calendar.setTimeInMillis(System.currentTimeMillis());
+//
+//                alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+//                        1000 * 5, alarmIntent);
+//                Log.d("txmail sync", "alarm registered");
+//
+//                v.setOnClickListener(stopButtonClickListener());
+//                Button thisButton = (Button)v;
+//                thisButton.setText(R.string.action_stop);
             }
         };
     }
@@ -71,17 +63,17 @@ public class StatusFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SyncService.class);
-                PendingIntent alarmIntent = PendingIntent.getBroadcast(v.getContext(), 0, intent, 0);
-                AlarmManager alarmMgr = (AlarmManager)v.getContext().getSystemService(Context.ALARM_SERVICE);
-
-                alarmMgr.cancel(alarmIntent);
-
-                Log.d("txmail sync", "alarm cancelled");
-
-                v.setOnClickListener(startButtonClickListener());
-                Button thisButton = (Button)v;
-                thisButton.setText(R.string.action_start);
+//                Intent intent = new Intent(v.getContext(), SyncService.class);
+//                PendingIntent alarmIntent = PendingIntent.getBroadcast(v.getContext(), 0, intent, 0);
+//                AlarmManager alarmMgr = (AlarmManager)v.getContext().getSystemService(Context.ALARM_SERVICE);
+//
+//                alarmMgr.cancel(alarmIntent);
+//
+//                Log.d("txmail sync", "alarm cancelled");
+//
+//                v.setOnClickListener(startButtonClickListener());
+//                Button thisButton = (Button)v;
+//                thisButton.setText(R.string.action_start);
             }
         };
     }
--- app/src/main/java/org/tbyte/txmail/SyncService.java
+++ app/src/main/java/org/tbyte/txmail/SyncService.java
@@ -3,16 +3,95 @@ package org.tbyte.txmail;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsMessage;
 import android.util.Log;
-import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Properties;
+
+import korex.mail.BodyPart;
+import korex.mail.MessagingException;
+import korex.mail.PasswordAuthentication;
+import korex.mail.Session;
+import korex.mail.Transport;
+import korex.mail.internet.InternetAddress;
+import korex.mail.internet.MimeBodyPart;
+import korex.mail.internet.MimeMessage;
+import korex.mail.internet.MimeMultipart;
 
 /**
  * Created by dgondos on 6/05/15.
  */
+
 public class SyncService extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Hitting alarm", Toast.LENGTH_SHORT).show();
-        Log.d("txmail sync", "alarm het");
+        Log.d("txmail", "alarm het");
+
+        byte[] pdu = (byte[]) ((Object[]) intent.getExtras().get("pdus"))[0];
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu);
+        String sms_orig_address = sms.getDisplayOriginatingAddress();
+        String sms_body = sms.getDisplayMessageBody();
+
+        Log.i("txmail", "received text from " + sms_orig_address + ": " + sms_body);
+        SMTPSender sender = new SMTPSender("192.168.1.6", "31331", "", "", false);
+        try {
+            sender.send("TxMail", "dgondos@localhost", "TxMail: New SMS from " + sms_orig_address, "SMS body:\n" + sms_body);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            Log.d("txmail", "Exception when trying to send mail: " + e.toString());
+        }
+    }
+}
+
+class SMTPSender extends korex.mail.Authenticator {
+    private String _user;
+    private String _pass;
+    private Properties _javamail_properties;
+
+    private boolean isAuthUsed() {
+        return !_user.isEmpty();
+    }
+
+    public SMTPSender(String host, String port, String user, String pass, boolean ssl) {
+        _user = user;
+        _pass = pass;
+
+        _javamail_properties = new Properties();
+
+        _javamail_properties.put("mail.smtp.host", host);
+        _javamail_properties.put("mail.smtp.auth", isAuthUsed());
+        _javamail_properties.put("mail.smtp.port", port);
+        if (ssl) {
+            _javamail_properties.put("mail.smtp.socketFactory.port", port);
+            _javamail_properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            _javamail_properties.put("mail.smtp.socketFactory.fallback", "false");
+        }
+    }
+
+    public void send(String from, String to, String subject, String body) throws MessagingException {
+        Session javamail_session = Session.getInstance(_javamail_properties, this);
+        MimeMessage mime_msg = new MimeMessage(javamail_session);
+
+        mime_msg.setFrom(new InternetAddress(from));
+        mime_msg.setSubject(subject);
+        mime_msg.setSentDate(new Date());
+
+        BodyPart bodyPart = new MimeBodyPart();
+        bodyPart.setText(body);
+        MimeMultipart content = new MimeMultipart();
+        content.addBodyPart(bodyPart);
+        mime_msg.setContent(content);
+
+        InternetAddress[] to_addresses = { new InternetAddress(to) };
+        mime_msg.setRecipients(MimeMessage.RecipientType.TO, to_addresses);
+
+        Transport.send(mime_msg);
+    }
+
+    @Override
+    public PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(_user, _pass);
     }
 }
